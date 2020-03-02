768d058c2cc1edf1855a586c18f14fd33356f74c
GTortorelli <gustavo.dra.tor@hotmail.com>
2020-03-02 08:32:18 -0300

    Lesson 3: Frog jump (100%)

--- FrogJmp/FrogJmp.py
+++ FrogJmp/FrogJmp.py
@@ -3,9 +3,11 @@
 # a função retorna a quantidade de pulos que o sapo precisa dar para sair da posição inicial e chegar no destino
 def solution(starting_position, goal_position, jump_distance):
     if(starting_position >= goal_position): return 0
+
     result = (goal_position - starting_position) / jump_distance
+
     if result % 1 !=0:
-        result = result+1
+        result = result + 1
 
     return int(result)
 
