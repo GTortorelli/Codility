51d8487b63a4ff1c834ebabfa2e7f62322238c49
GTortorelli <gustavo.dra.tor@hotmail.com>
2020-03-03 10:51:28 -0300

    Lesson 4: Permutation Check (100%)

---
+++ Permutation Check/PermCheck.py
@@ -0,0 +1,19 @@
+# Permutation check - Codility
+# 03 - 03 - 2020
+
+# The method must be capable to check if the array is a permutation :D
+
+def solution(array:list):
+
+    array = sorted(array)
+
+    if array[0] != 1: return 0
+
+    j = array[0]
+
+    for i in array:
+        if i != j: return 0
+
+        j += 1
+
+    return 1
