612119ebef097ab1eb48da61fa95f43c943b7c6b
GTortorelli <gustavo.dra.tor@hotmail.com>
2020-08-26 11:48:35 -0300

    adjusting syntax

--- Permutation Check/PermCheck.py
+++ Permutation Check/PermCheck.py
@@ -3,17 +3,16 @@
 
 # The method must be capable to check if the array is a permutation :D
 
+
 def solution(array:list):
-
     array = sorted(array)
-
-    if array[0] != 1: return 0
+    if array[0] != 1:
+        return 0
 
     j = array[0]
-
     for i in array:
-        if i != j: return 0
-
+        if i != j:
+            return 0
         j += 1
 
     return 1
